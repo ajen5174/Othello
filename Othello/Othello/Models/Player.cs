5a54f6c56f3c0c0dc24a6fcd23d4eb07e756bfbf
Ragger15 <carsonsena@gmail.com>
2020-05-21 10:12:37 -0600

    Add base class for Board, Stone, and Player classes

---
+++ Othello/Othello/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello.Models
+{
+    class Player
+    {
+        public bool PlayerNumber { get; }
+        public int OwnedStones { get; set; } = 0;
+
+        public Player(bool playerNumber)
+        {
+            PlayerNumber = playerNumber;
+        }
+    }
+}
