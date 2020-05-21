f21fdd860cedf46dd846fdf24c20306edda5faa3
Andrew Jensen <AJensen@student.neumont.edu>
2020-05-21 10:51:41 -0600

    add unit test project

---
+++ Othello/OthelloTesting/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OthelloTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Equals()
+        {
+            Assert.AreEqual(10, 10);
+        }
+
+        [TestMethod]
+        public void NotEquals()
+        {
+            Assert.AreNotEqual(11, 10);
+        }
+    }
+}
